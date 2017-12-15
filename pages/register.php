89729d22a839d4585709c91bd71e1fbaace288fe
Sanket Nandakumar Pednekar <sp2363@afsconnect2.njit.edu>
2017-12-14 23:39:30 -0500

    Update the register page

--- pages/register.php
+++ pages/register.php
@@ -23,7 +23,6 @@
     Last name: <input type="text" name="lname"><br>
     Email: <input type="text" name="email"><br>
     Phone: <input type="text" name="phone"><br>
-    Birthday: <input type="text" name="birthday"><br>
     Gender: <input type="text" name="gender"><br>
     Password: <input type="password" name="password"><br>
     <input type="submit" value="Submit form">
