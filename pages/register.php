833f8410ec53b32e0c94fd8b8ca342b034c50bf3
Sanket Nandakumar Pednekar <sp2363@afsconnect2.njit.edu>
2017-12-15 01:16:36 -0500

    minor changes in register page

--- pages/register.php
+++ pages/register.php
@@ -18,7 +18,7 @@
 <body>
 
 
-<form action="index.php?page=accounts&action=register" method="post">
+<form action="index.php?page=accounts&action=register" method="post" id="form1">
     First name: <input type="text" name="fname"><br>
     Last name: <input type="text" name="lname"><br>
     Email: <input type="text" name="email"><br>
