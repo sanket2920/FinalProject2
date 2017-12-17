9a5b8a2f45c3915d4d4af54af039d4b66733863e
Sanket Nandakumar Pednekar <sp2363@afsconnect2.njit.edu>
2017-12-17 16:42:03 -0500

    Worked on page that controls the http

--- core/http/controller.php
+++ core/http/controller.php
@@ -1,11 +1,8 @@
 <?php
-
 namespace http;
 //this is the controller class that you use to connect models with views and business logic
 class controller
 {
-
-
 //this gets the HTML template for the application and accepts the model.  The model array can be used in the template
     static public function getTemplate($template, $data = NULL)
     {
@@ -13,6 +10,6 @@ class controller
         $template = 'pages/' . $template . '.php';
 //in your template you should use $data to access your array
         include $template;
-
     }
 }
+?>
