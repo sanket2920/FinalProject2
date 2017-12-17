0ff61268ea652e0efa6edb81d528ee47e2772558
Sanket Nandakumar Pednekar <sp2363@afsconnect2.njit.edu>
2017-12-17 00:07:11 -0500

    Updated the page that controls the todo tasks

--- controllers/tasksController.php
+++ controllers/tasksController.php
@@ -1,13 +1,4 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: kwilliams
- * Date: 11/27/17
- * Time: 5:32 PM
- */
-
-
-//each page extends controller and the index.php?page=tasks causes the controller to be called
 class tasksController extends http\controller
 {
     //each method in the controller is named an action.
@@ -17,31 +8,28 @@ class tasksController extends http\controller
         $record = todos::findOne($_REQUEST['id']);
         self::getTemplate('show_task', $record);
     }
-
     //to call the show function the url is index.php?page=task&action=list_task
-
     public static function all()
     {
-        $records = todos::findAll();
-        /*session_start();
-           if(key_exists('userID',$_SESSION)) {
-               $userID = $_SESSION['userID'];
-           } else {
-
+        //$records = todos::findAll();
+	//self::getTemplate('all_tasks', $records);
+	$records = todos::findAll();
+	//print_r($records);
+        session_start();
+        if(key_exists('userID',$_SESSION)) {
+               	$userID = $_SESSION['userID'];
+	      	$userID = $_SESSION['userID'];
+	       	$records = todos::findTasksbyID($userID);
+		self::getTemplate('all_tasks', $records);
+      	} else {
                echo 'you must be logged in to view tasks';
            }
-        $userID = $_SESSION['userID'];
-
-        $records = todos::findTasksbyID($userID);
-        */
-        self::getTemplate('all_tasks', $records);
-
+        //session_start();
+	//echo $_SESSION['userID'];
     }
     //to call the show function the url is called with a post to: index.php?page=task&action=create
     //this is a function to create new tasks
-
     //you should check the notes on the project posted in moodle for how to use active record here
-
     public static function create()
     {
         //print_r($_POST);
@@ -51,33 +39,34 @@ class tasksController extends http\controller
     public static function edit()
     {
         $record = todos::findOne($_REQUEST['id']);
-
         self::getTemplate('edit_task', $record);
-
     }
-
     //this would be for the post for sending the task edit form
     public static function store()
     {
-
-
-        $record = todos::findOne($_REQUEST['id']);
-        $record->body = $_REQUEST['body'];
-        $record->save();
-        print_r($_POST);
-
+	$id=$_REQUEST['id'];
+	         if($id==null){
+	  		$record=new \todo;
+	             	$record->owneremail=$_POST['owneremail'];
+	             	$record->ownerid=$_POST['ownerid'];
+	       	        $record->createddate=$_POST['createddate'];
+	                $record->duedate=$_POST['duedate'];
+	                $record->message=$_POST['message'];
+	                $record->isdone=$_POST['isdone'];
+		        $record->save(); 
+	       }
+		           else{
+		              $record = todos::findOne($_REQUEST['id']);
+		               $record->owneremail=$_POST['owneremail'];
+		               $record->ownerid=$_POST['ownerid'];
+		                 $record->createddate=$_POST['createddate'];
+		                  $record->duedate=$_POST['duedate'];
+			           $record->message=$_POST['message'];
+			            $record->isdone=$_POST['isdone'];
+			             $record->save();
+			              }
+			             header('Location:index.php?page=tasks&action=all');
     }
-
-    public static function save() {
-        session_start();
-        $task = new todo();
-
-        $task->body = $_POST['body'];
-        $task->ownerid = $_SESSION['userID'];
-        $task->save();
-
-    }
-
     //this is the delete function.  You actually return the edit form and then there should be 2 forms on that.
     //One form is the todo and the other is just for the delete button
     public static function delete()
@@ -86,8 +75,7 @@ class tasksController extends http\controller
         $record->delete();
         $records=todos:findAll();
 	self:getTemplate('all_tasks',$records);
-	//print_r($_POST);
-
+	header('Location:index.php?page=tasks&action=all');
     }
-
 }
+?>
