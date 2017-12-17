b3ff39789625920188e18486ce17915543103f97
Sanket Nandakumar Pednekar <sp2363@afsconnect2.njit.edu>
2017-12-16 23:44:11 -0500

    developed a page to show new todo item

--- pages/show_task.php
+++ pages/show_task.php
@@ -4,10 +4,14 @@
 <head>
     <meta charset="utf-8">
 
-    <title>The HTML5 Herald</title>
-    <meta name="description" content="The HTML5 Herald">
-    <meta name="author" content="SitePoint">
-
+    	<title>The HTML5 Herald</title>
+    	<meta name="description" content="The HTML5 Herald">
+    	<meta name="author" content="SitePoint">
+	
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+	    
     <link rel="stylesheet" href="css/styles.css?v=1.0">
 
     <!--[if lt IE 9]>
@@ -18,16 +22,42 @@
 <body>
 
 <?php
-//this is how you print something  $data contains the record that was selected on the table.
-
-print_r($data);
+	echo '<br>';
 ?>
-<form action="index.php?page=tasks&action=delete&id=<?php echo $data->id; ?> " method="post" id="form1">
-    <button type="submit" form="form1" value="delete">Delete</button>
-</form>
 
+<center>
+	<form action="index.php?page=tasks&action=store&id=<?php echo $data->id; ?>" method="post" class = "form-inline" id="form1">
+		<div class = "form-group">
+			ID:<input type="text" class="form-control" name="id" value="<?php echo $data->id;?>">
+		</div><br><br>
 
+		<div class = "form-group">
+			Owner Email:<input type="text" class="form-control" name="owneremail" value="<?php echo $data->owneremail;?>">
+		</div><br><br>
 
+		<div class = "form-group">
+			Owner ID:<input type="text" class="form-control" name="ownerid" value="<?php echo $data->ownerid;?>">
+		</div><br><br>
+
+		<div class = "form-group">
+			Created Date:<input type="text" class="form-control" name="createddate" value="<?php echo $data->createddate;?>">
+		</div><br><br>
+
+		<div class = "form-group">
+			Due Date:<input type="text" class="form-control" name="duedate" value="<?php echo $data->duedate;?>">
+		</div><br><br>
+
+		<div class = "form-group">
+			Message:<input type="text" class="form-control" name="message" value="<?php echo $data->message;?>">
+		</div><br><br>
+
+		<div class = "form-group">
+			Is Done:<input type="text" class="form-control" name="isdone" value="<?php echo $data->isdone;?>">
+		</div><br><br>
+
+		<button type="submit" class="btn btn-primary" form="form1" value="save">Save</button>
+	</form>
+</center>
 
 <script src="js/scripts.js"></script>
 </body>
